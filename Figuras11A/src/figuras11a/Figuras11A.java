a764137ced65c5504a4dbb4f786795288580a415
Enrique Garcia <enrique.garciah@gmail.com>
2019-01-21 18:25:35 +0100

    Modificada variable cont. Nuevo nombre: asteriscos

--- Figuras11A/src/figuras11a/Figuras11A.java
+++ Figuras11A/src/figuras11a/Figuras11A.java
@@ -12,20 +12,20 @@ import java.util.*;
 public class Figuras11A {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int filas, i, j, k, cont;
+        int filas, i, j, k, asteriscos;
         do{
             System.out.print("Introduce número de filas: ");
             filas = sc.nextInt();
         }while(filas<=0);
-        cont=1; // Esta variable contiene el número de asteriscos que se escriben en cada fila
+        asteriscos=1; // Esta variable contiene el número de asteriscos que se escriben en cada fila
 	for(i=1;i<=filas;i++){  // Total de filas de la pirámide. Para cada fila.....
 	    for(j=1;j<=filas-i;j++){  // Escribimos primero los blancos. En cada fila i hay filas-i blancos
 		System.out.print(" ");
 	    }
-	    for(k=1;k<=cont;k++){       // A continuación escribimos los asteriscos. En la primera fila hay solo uno
+	    for(k=1;k<=asteriscos;k++){       // A continuación escribimos los asteriscos. En la primera fila hay solo uno
 		System.out.print("*");
             }
-            cont+=2; // en cada fila hay 2 asteriscos más que en la anterior
+            asteriscos+=2; // en cada fila hay 2 asteriscos más que en la anterior
 	    System.out.println();//Salto de línea
 	}
     }
