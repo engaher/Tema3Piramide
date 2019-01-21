be113c339bfc4907e503b3a7fbeac0813f1f9d4c
Enrique Garcia <enrique.garciah@gmail.com>
2019-01-21 18:20:38 +0100

    proyecto inicial

---
+++ Figuras11A/src/figuras11a/Figuras11A.java
@@ -0,0 +1,32 @@
+package figuras11a;
+import java.util.*;
+/**
+ * Figuras11A
+ * Programa que muestre las siguiente figura
+ *       *
+ *      ***
+ *     *****
+ *    *******
+ *   *********      la altura se lee por teclado
+ */
+public class Figuras11A {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int filas, i, j, k, cont;
+        do{
+            System.out.print("Introduce número de filas: ");
+            filas = sc.nextInt();
+        }while(filas<=0);
+        cont=1; // Esta variable contiene el número de asteriscos que se escriben en cada fila
+	for(i=1;i<=filas;i++){  // Total de filas de la pirámide. Para cada fila.....
+	    for(j=1;j<=filas-i;j++){  // Escribimos primero los blancos. En cada fila i hay filas-i blancos
+		System.out.print(" ");
+	    }
+	    for(k=1;k<=cont;k++){       // A continuación escribimos los asteriscos. En la primera fila hay solo uno
+		System.out.print("*");
+            }
+            cont+=2; // en cada fila hay 2 asteriscos más que en la anterior
+	    System.out.println();//Salto de línea
+	}
+    }
+}
